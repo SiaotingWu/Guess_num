8f90e4801c74defe01a75b57026f03f065015fc9
Siaoting Wu <qoo0800336648@gmail.com>
2022-06-01 15:38:10 +0800

    增加顯示猜題次數

--- 終極密碼.py
+++ 終極密碼.py
@@ -6,14 +6,18 @@
 import random
 
 r = random.randint(1, 100)
+count = 0 #為了計算猜幾次
 while True:
+	count += 1  # count = count + 1 
 	s = input('請猜數字(1~100)： ')
 	s = int(s)
 	if s == r:
 		print('恭喜猜對了！')
+		print('總共猜了', count,'次')
 		break
 	elif s > r:
 		print('比答案大')
 	else:
 		print('比答案小')
-
+	print('這是你猜的第', count,'次')
+	
